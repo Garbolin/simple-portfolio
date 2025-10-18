acf66b21c968c0f7198eceee304af195c2b22dc0
Gabriela Buil <134524103+Garbolin@users.noreply.github.com>
2025-10-18 18:53:37 +0200

    MP-FixStyles: fade-in done

--- JS/main.js
+++ JS/main.js
@@ -3,3 +3,32 @@
 document.addEventListener("DOMContentLoaded", () => {
     loadLanguage('es');
 })
+
+const sections = document.querySelectorAll('section');
+const header = document.querySelectorAll('header');
+const home = document.querySelectorAll('#home');
+
+
+    const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+        entry.target.style.animationPlayState = 'running';
+        observer.unobserve(entry.target);
+        }
+    });
+    }, { threshold: 0.2 });
+
+    sections.forEach(section => {
+    section.style.animationPlayState = 'paused';
+    observer.observe(section);
+    });
+
+    header.forEach(header => {
+    header.style.animationPlayState = 'paused';
+    observer.observe(header);  // Corregido: observar cada div individualmente
+    });
+
+    home.forEach(home => {
+    home.style.animationPlayState = 'paused';
+    observer.observe(home);  // Corregido: observar cada div individualmente
+    });
